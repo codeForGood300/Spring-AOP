ee73c61ec9ed76321415e8dcc6aea1a0d7911880
codeForGood300 <vardhan2011harsh@gmail.com>
2022-09-11 15:15:02 +0530

    Spring AOP concept- Aspect

---
+++ src/main/java/io/datajek/springaop/movierecommenderaop/data/Movie.java
@@ -0,0 +1,10 @@
+package io.datajek.springaop.movierecommenderaop.data;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Movie {
+
+    public String getMovieDetails() {
+        return "movie details";
+    }
+}
