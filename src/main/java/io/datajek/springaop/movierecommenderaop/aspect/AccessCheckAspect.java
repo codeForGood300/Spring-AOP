a3f5020149b2ad5cb4677326718e6cf9e21a25dc
codeForGood300 <vardhan2011harsh@gmail.com>
2022-09-11 15:49:11 +0530

    Spring AOP - jointpoint

--- src/main/java/io/datajek/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
+++ src/main/java/io/datajek/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
@@ -13,7 +13,11 @@ public class AccessCheckAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
+    @Before("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..)) || execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
+    //@Before("execution(String io.datajek.springaop.movierecommenderaop..*.*(String))")
+    //@Before("execution(* io.datajek.springaop.movierecommenderaop..*.*Filtering(..))")
+    //@Before("execution(String io.datajek.springaop.movierecommenderaop..*.*(..))")
+    //@Before("execution(* io.datajek.springaop.movierecommenderaop.business.*.*(..))")
     public void before(JoinPoint joinPoint) {
         //	logger.info("Intercepted method call");
         logger.info("Intercepted call before execution of: {}", joinPoint);
